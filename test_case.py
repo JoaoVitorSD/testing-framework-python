680e922a722ec9f066e95c8f1143f4c6a5f4c391
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2025-05-03 00:05:04 -0300

    3. Classe TestResult

--- test_case.py
+++ test_case.py
@@ -1,9 +1,11 @@
+from test_result import TestResult
 class TestCase:
 
     def __init__(self, test_method_name):
         self.test_method_name = test_method_name
 
-    def run(self):
+    def run(self, result:TestResult):
+        result.test_started()
         self.set_up()    # chama método de setup
         test_method = getattr(self, self.test_method_name)
         test_method()    # chama método de teste 
