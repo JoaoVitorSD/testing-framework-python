2c45efe3c7114b414ac9c1b28ffaadb867dc15a7
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2025-05-03 00:23:44 -0300

    4. Testando TestCase

--- test_case.py
+++ test_case.py
@@ -1,4 +1,5 @@
 from test_result import TestResult
+
 class TestCase:
 
     def __init__(self, test_method_name):
@@ -6,10 +7,15 @@ class TestCase:
 
     def run(self, result:TestResult):
         result.test_started()
-        self.set_up()    # chama método de setup
-        test_method = getattr(self, self.test_method_name)
-        test_method()    # chama método de teste 
-        self.tear_down() # chama método de teardown 
+        self.set_up()
+        try:
+            test_method = getattr(self, self.test_method_name)
+            test_method()
+        except AssertionError as e:
+            result.add_failure(self.test_method_name)
+        except Exception as e:
+            result.add_error(self.test_method_name)
+        self.tear_down()
 
     def set_up(self):
         pass
